8f280b7cac1a8287da75b8a0fe08d7e8cc2e8726
arberlisaj <the.arberlisaj@gmail.com>
2024-11-04 18:31:42 +0100

    just messin around tbh

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,16 @@
 import UsersTable from '@/components/UsersTable';
+import useUsers from '@/hooks/useUsers';
 
 const Dashboard = () => {
+  const { user } = useUsers('/1');
+  const { users } = useUsers();
+
   return (
-    <div className="overflow-x-scroll">
-      <UsersTable />
+    <div>
+      <h1>Fetch a single user</h1>
+      <h1>{user?.name}</h1>
+      <h1>Fetch all users and display them in a table</h1>
+      <UsersTable users={users} />
     </div>
   );
 };
