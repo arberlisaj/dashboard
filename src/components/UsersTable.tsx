b0f278b0b98a65433159eddcf3c37343aa6a560b
arberlisaj <the.arberlisaj@gmail.com>
2024-11-04 18:29:25 +0100

    refactor: separate data fetching logic, now solely responsible for displaying user data

--- src/components/UsersTable.tsx
+++ src/components/UsersTable.tsx
@@ -1,34 +1,30 @@
-import useUsers from '@/hooks/useUsers';
+import { User } from '@/services/user-service';
 
-const UsersTable = () => {
-  const { users, isLoading, error } = useUsers();
-  if (error) return <p>Oops! Something went wrong!</p>;
-  else if (isLoading) return <p>Loading...</p>;
-  else
-    return (
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Website</th>
-            <th>City</th>
+const UsersTable = ({ users }: { users: User[] }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Website</th>
+          <th>City</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user) => (
+          <tr key={user.id}>
+            <td className="text-center">{user.id}</td>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+            <td>{user.website}</td>
+            <td>{user.address.city}</td>
           </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user.id}>
-              <td className="text-center">{user.id}</td>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-              <td>{user.website}</td>
-              <td>{user.address.city}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    );
+        ))}
+      </tbody>
+    </table>
+  );
 };
 
 export default UsersTable;
